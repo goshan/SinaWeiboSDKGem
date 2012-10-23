a10271a4389beea85e25c2d1ad36e2c609ad712a
qiuhan <goshan.hanqiu@gmail.com>
2012-10-23 10:48:25 +0800

    version 0.1.1: a little change

--- lib/sina.rb
+++ lib/sina.rb
@@ -7,7 +7,7 @@
 require 'sina/config'
 require 'sina/base'
 if File.exists?('config/sina.yml')
-  weibo_oauth = YAML.load_file('config/weibo.yml')[Rails.env || env || 'development']
+  weibo_oauth = YAML.load_file('config/sina.yml')[Rails.env || env || 'development']
   Sina::Config.app_key = weibo_oauth["app_key"]
-  Sina::Config.scret_key = weibo_oauth["scret_key"]
+  Sina::Config.secret_key = weibo_oauth["secret_key"]
 end
